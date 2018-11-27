3a8b42b7ff992a8255ff196e418cb510ff6eb470
Ali Taghizadeh <ali.taghizadeh@outlook.com>
2018-11-27 19:52:32 +0330

    create a dependency registry class for injecting layers

---
+++ app/src/main/java/ir/taghizadeh/android_mvp/dependencies/DependencyRegistry.java
@@ -0,0 +1,45 @@
+package ir.taghizadeh.android_mvp.dependencies;
+
+import com.google.gson.Gson;
+import io.realm.Realm;
+import ir.taghizadeh.android_mvp.model.conversionLayer.Conversion;
+import ir.taghizadeh.android_mvp.model.conversionLayer.ConversionLayer;
+import ir.taghizadeh.android_mvp.model.conversionLayer.HomeConverter;
+import ir.taghizadeh.android_mvp.model.conversionLayer.HomeConverterImpl;
+import ir.taghizadeh.android_mvp.model.dataLayer.Data;
+import ir.taghizadeh.android_mvp.model.dataLayer.DataLayer;
+import ir.taghizadeh.android_mvp.model.modelLayer.Model;
+import ir.taghizadeh.android_mvp.model.modelLayer.ModelLayer;
+import ir.taghizadeh.android_mvp.model.networkLayer.Network;
+import ir.taghizadeh.android_mvp.model.networkLayer.NetworkLayer;
+
+public class DependencyRegistry {
+
+    public static  DependencyRegistry register = new DependencyRegistry();
+
+    private Gson gson = new Gson();
+    private Realm realm = Realm.getDefaultInstance();
+    private Realm newRealmInstanceOnCurrentThread() {
+        return Realm.getInstance(realm.getConfiguration());
+    }
+
+    private HomeConverter homeConverter = new HomeConverterImpl();
+
+    private ConversionLayer conversionLayer = createConversionLayer();
+    private ConversionLayer createConversionLayer() {
+        return new Conversion(gson, homeConverter);
+    }
+
+    private DataLayer dataLayer = createDataLayer();
+    private DataLayer createDataLayer() {
+        return new Data(realm, this::newRealmInstanceOnCurrentThread);
+    }
+
+    private NetworkLayer networkLayer = new Network();
+
+    private ModelLayer modelLayer = createModelLayer();
+    private ModelLayer createModelLayer() {
+        return new Model(networkLayer, dataLayer, conversionLayer);
+    }
+
+}
